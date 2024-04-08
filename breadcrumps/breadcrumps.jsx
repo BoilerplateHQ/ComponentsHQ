b33186ba7cabd17c08619f40818b49ed2ba5cbbc
Till <tkahlen@gmail.com>
2024-04-08 17:09:15 +0200

    Optimized: Breadcrump on mobile with many sub-folders

--- breadcrumps/breadcrumps.jsx
+++ breadcrumps/breadcrumps.jsx
@@ -14,33 +14,33 @@ export default function Breadcrumps() {
 	const pathArray = pathname.split('/');
 
 	return (
-		<div className="flex items-center py-4 text-sm text-muted-foreground space-x-2 bg-white w-[400px] h-[250px] justify-center">
-			<div className="text-ellipsis whitespace-nowrap">
-				<Link href="/" aria-describedby="home button">
-					<HomeIcon className="h-4 w-4" />
-				</Link>
-			</div>
-			{pathArray.length > 1
-				? pathArray.slice(1).map((path, index) => (
-						<div key={path} className="flex items-center">
-							<ChevronRight className="h-4 w-4" />
-							<div
-								className={cn(
-									pathArray.length - 2 === index
-										? 'font-medium text-foreground'
-										: 'text-sm text-muted-foreground'
-								)}
-							>
-								<Link href={pathArray.slice(0, index + 2).join('/')}>
-									{path
-										.split('-')
-										.map((w) => capitalize(w))
-										.join(' ')}
-								</Link>
+		<div className="flex items-center py-4 text-sm text-muted-foreground">
+			<Link href="/" className="px-2 sm:px-0" aria-describedby="home button">
+				<HomeIcon className="h-5 w-5 sm:h-4 sm:w-4" />
+			</Link>
+			<div className="flex flex-wrap">
+				{pathArray.length > 1
+					? pathArray.slice(1).map((path, index) => (
+							<div key={path} className="flex items-center leading-10">
+								<ChevronRight className="h-4 w-4" />
+								<div
+									className={cn(
+										pathArray.length - 2 === index
+											? 'font-medium text-foreground'
+											: 'text-sm text-muted-foreground'
+									)}
+								>
+									<Link href={pathArray.slice(0, index + 2).join('/')}>
+										{path
+											.split('-')
+											.map((w) => capitalize(w))
+											.join(' ')}
+									</Link>
+								</div>
 							</div>
-						</div>
-				  ))
-				: null}
+					  ))
+					: null}
+			</div>
 		</div>
 	);
 }
